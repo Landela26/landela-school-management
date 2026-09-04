72a866c02c3451f278468fd6dacd91a3c594cf5b
H.Lite <155548326+Henock-Lite@users.noreply.github.com>
2026-09-04 02:58:40 +0200

    Revert "Fix/login style tailwind (#12)
    This reverts commit 6b31b1428073280d5b1dbafc3592c6587aab9f98.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-// Route::post('/auth/login', [AuthController::class, 'login']);
